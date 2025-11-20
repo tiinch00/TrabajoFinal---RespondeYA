004fba6aa7fa52b401052e902aa6c43f79efaacb
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2025-11-19 23:16:09 -0300

    agregue 'https://trabajo-final-responde-ya.vercel.app' en allowedOrigins (linea 38)

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   // cuando tengas el dominio / Vercel, los agregás acá:
-  // 'https://respondeya-frontend.vercel.app',
+  'https://trabajo-final-responde-ya.vercel.app', // dominio real de Vercel
   // 'https://api.tudominio.com',
 ];
 
