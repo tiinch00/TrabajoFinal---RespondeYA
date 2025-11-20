0d1019fe8d522f5f37a24aebd7a6125e2b842345
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2025-11-20 01:46:03 -0300

    mejora de socket.io

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -7,36 +7,34 @@ import registrarEventosSala from './sala.controller.js';
 let ioRef = null;
 
 export function crearSocketServer(httpServer) {
+    const allowedOrigins = [
+        'https://proyecto-respondeya.vercel.app',
+        'https://respondeya.website',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ];
+
     const io = new Server(httpServer, {
         path: '/socket.io',
         cors: {
-            origin: [
-                "http://localhost:5173",
-                "http://127.0.0.1:5173",
-                "http://52.15.53.177:3000",
-                "http://52.15.53.177",
-                //"https://respondeya.vercel.app",
-                //"https://api.respondeya.com",
-            ],
-            methods: ["GET", "POST"],
-            credentials: true
+            origin: allowedOrigins,
+            methods: ['GET', 'POST'],
+            credentials: true,
         },
+        transports: ['websocket', 'polling'], // opcional, pero prolijo
     });
 
     io.on('connection', (socket) => {
         console.log('🔌 socket conectado:', socket.id);
 
-        // registrar todos los “controllers” de sockets
         registrarEventosSala(io, socket);
         registrarEventosChat(io, socket);
-        // acá podrías registrar otros: registrarEventosChat(io, socket); etc.
     });
 
     ioRef = io;
     return io;
 }
 
-// Para emitir desde controllers HTTP (e.g., después de guardar en DB)
 export function getIO() {
     if (!ioRef) throw new Error('Socket.IO no inicializado');
     return ioRef;
